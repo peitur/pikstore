fe02383440edf816c9b8b4881e24fe45b14d11b6
peitur <peitur@gmail.com>
2019-01-20 22:20:56 +0100

    started work on command manager

---
+++ piks/db.py
@@ -0,0 +1,23 @@
+ #!/usr/bin/env python3
+
+import os, sys, re
+import json
+import sqlite
+
+import piks
+import piks.utils
+import piks.defaults
+import piks.validate
+
+from pprint import pprint
+
+class Command( object ):
+
+    def __init__( self, argv, **opt ):
+        pass
+
+    def run( self ):
+        pass
+
+if __name__ == "__main__":
+    pass
--- piks/runner.py
+++ piks/runner.py
@@ -29,7 +29,6 @@ class GenericCommand( object ):
         result = list()
         cmd = self._cmd
 
-
         if type( cmd ).__name__ == "str":
             cmd = shlex.split( cmd )
 
@@ -41,3 +40,50 @@ class GenericCommand( object ):
         for line in prc.stdout.readlines():
             result.append( line.lstrip().rstrip() )
         return result
+
+
+class CreatePrivateKey( GenericCommand ):
+
+    def __init__( self, **opt ):
+        self._debug = False
+        self._password = opt['password']
+        self._algorithm = opt['algorithm']
+        self._bits = "4096"
+        self._encode = "des3"
+
+        if 'debug' in opt and opt['debug'] in (True,False):
+            self._debug = opt['debug']
+
+        if 'encode' in opt and opt['encode'] in ("des3","aes128","aes192", "aes256","camellia128","camellia192","camellia256")
+            self._encode = opt['encode']
+
+        command = [ "genrsa",
+            "-%s" % ( self._encode ),
+            "-passout", self._password,
+            self._bits
+        ]
+
+        super().__init__( command, debug=self._debug )
+
+
+    def run( self ):
+        pass
+
+    def get_privatekey( self ):
+        return self._pkey
+
+
+class CreatePublicKey( GenericCommand ):
+
+    def __init__( self, **opt ):
+        super().__init__()
+        self._password = opt['password']
+        self._algorith = opt['algorith']
+        self._pkey = opt['privatekey']
+
+    def run( self ):
+        pass
+
+
+if __name__ == "__main__":
+    pprint( CreatePrivateKey( "test" ).run() )
